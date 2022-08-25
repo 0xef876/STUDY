731445ef257417c6fd2e5d89c8f5f61bb1c6ae6c
kitty <kitty@parksuncbookpro>
2022-08-25 19:28:36 +0900

    220825 Commit

---
+++ BECKJOON/src/kitty/Date_220825_BOJ_1008.java
@@ -0,0 +1,19 @@
+/*
+ * A / B
+ * https://www.acmicpc.net/problem/1008
+ */
+package kitty;
+
+import java.util.Scanner;
+
+public class Date_220825_BOJ_1008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		System.out.println((double) a/b);
+	}
+}
