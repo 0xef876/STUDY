c912853b5ad4fdf0aa904a30b8a871e39aa7a5a4
kitty <kitty@parksuncbookpro>
2022-08-25 16:33:15 +0900

    220825 Commit

---
+++ BECKJOON/src/kitty/Date_220825_BOJ_1000.java
@@ -0,0 +1,10 @@
+package kitty;
+
+public class Date_220825_BOJ_1000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
---
+++ BECKJOON/src/kitty/Date_220825_BOJ_2557.java
@@ -0,0 +1,10 @@
+package kitty;
+
+public class Date_220825_BOJ_2557 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
