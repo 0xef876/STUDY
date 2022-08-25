5c59c4edeb5b021b8d4849058a6fa14d5f8578c7
kitty <kitty@parksuncbookpro>
2022-08-25 18:01:43 +0900

    Revert "220825 Commit"
    This reverts commit 8138ea3e3db9f3e004b21b8b2e881b33f0a93ce6.

--- BECKJOON/src/kitty/Date_220825_BOJ_1000.java
+++ BECKJOON/src/kitty/Date_220825_BOJ_1000.java
@@ -1,8 +1,3 @@
-/*
- * A+B
- * https://www.acmicpc.net/problem/1000
- */
-
 package kitty;
 
 import java.util.Scanner;
--- BECKJOON/src/kitty/Date_220825_BOJ_2557.java
+++ BECKJOON/src/kitty/Date_220825_BOJ_2557.java
@@ -1,7 +1,3 @@
-/*
- * Hello World
- * https://www.acmicpc.net/problem/2557
- */
 package kitty;
 
 public class Date_220825_BOJ_2557 {
