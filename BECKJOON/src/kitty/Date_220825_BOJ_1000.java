5e197a5c0e48b388f27f10049e115c64fab2e496
kitty <kitty@parksuncbookpro>
2022-08-25 16:49:37 +0900

    220825 Commit

--- BECKJOON/src/kitty/Date_220825_BOJ_1000.java
+++ BECKJOON/src/kitty/Date_220825_BOJ_1000.java
@@ -1,10 +1,17 @@
 package kitty;
 
+import java.util.Scanner;
+
 public class Date_220825_BOJ_1000 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-	}
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		System.out.println(a+b);
+		}
 
 }
