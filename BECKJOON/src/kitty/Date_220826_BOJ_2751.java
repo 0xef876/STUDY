e4a3cefb3869354edb402a55425b3ba6b0563b7e
kitty <kitty@parksuncbookpro>
2022-08-27 15:54:57 +0900

    220826 Commit

--- BECKJOON/src/kitty/Date_220826_BOJ_2751.java
+++ BECKJOON/src/kitty/Date_220826_BOJ_2751.java
@@ -12,28 +12,6 @@ public class Date_220826_BOJ_2751 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i+1; j < arr.length; j++) {
-				if (arr[i] > arr[j])
-				{
-					int temp = arr[j];
-					arr[j] = arr[i];
-					arr[i] = temp;
-				}
-					
-			}
-		}
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
-		}
-	*/
 		
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
