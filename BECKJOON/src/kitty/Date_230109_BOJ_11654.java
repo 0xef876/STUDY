6a165425821f2ea9d2523405450393e62736d151
PARK SUNG JUN <kittypark.github.io@gmail.com>
2023-01-09 10:52:33 +0900

    Create Date_230109_BOJ_11654.java
    solved BOJ 11654

---
+++ BECKJOON/src/kitty/Date_230109_BOJ_11654.java
@@ -0,0 +1,11 @@
+/*
+ BOJ 11654
+*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.nextLine().charAt(0);
+        System.out.print((int)ch);
+    }
+}
