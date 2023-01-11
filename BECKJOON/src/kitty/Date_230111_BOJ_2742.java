0e190104e9e789eebc755f8b2fa87989c22d5649
PARK SUNG JUN <kittypark.github.io@gmail.com>
2023-01-11 16:52:50 +0900

    Create Date_230111_BOJ_2742.java
    Solved BOJ 2742

---
+++ BECKJOON/src/kitty/Date_230111_BOJ_2742.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// BOJ 2742
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = N; i > 0; i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
