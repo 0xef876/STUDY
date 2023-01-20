2f2a6b562b746fef70d39b7c404b950fac92eaa2
PARK SUNG JUN <kittypark.github.io@gmail.com>
2023-01-20 02:14:03 +0000

    Modified Comments

--- sql/Date_230120_Find_sick_animal.sql
+++ sql/Date_230120_Find_sick_animal.sql
@@ -1,5 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/59036
+--https://school.programmers.co.kr/learn/courses/30/lessons/59036
 
--- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
 WHERE INTAKE_CONDITION = 'Sick';
