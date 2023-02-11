2c3aa732b7980492c5557c4771b3a7f76b9d2ed3
PARK SUNG JUN <kittypark.github.io@gmail.com>
2023-02-11 15:50:04 +0900

    doce : BOJ 11726

---
+++ python/Date_230211_BOJ_11726.py
@@ -0,0 +1,13 @@
+import sys
+def solution(n):
+    dp = [0] * (n+1)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    if n == 1 or n == 2:
+        return dp[n]
+    else:
+        for i in range(3,len(dp)):
+            dp[i] = dp[i-2] + dp[i-1]
+        return dp[-1] % 10007
+print(solution(int(sys.stdin.readline().rstrip())))
