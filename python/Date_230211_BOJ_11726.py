30298108bd946515fcba1f51447673ceb9ff082b
PARK SUNG JUN <kittypark.github.io@gmail.com>
2023-02-11 15:59:30 +0900

    docs : BOJ 11726

--- python/Date_230211_BOJ_11726.py
+++ python/Date_230211_BOJ_11726.py
@@ -1,6 +1,6 @@
 import sys
 def solution(n):
-    dp = [0] * (n+1)
+    dp = [0] * (1001)
     dp[0] = 0
     dp[1] = 1
     dp[2] = 2
@@ -9,5 +9,5 @@ def solution(n):
     else:
         for i in range(3,len(dp)):
             dp[i] = dp[i-2] + dp[i-1]
-        return dp[-1] % 10007
+        return dp[n] % 10007
 print(solution(int(sys.stdin.readline().rstrip())))
