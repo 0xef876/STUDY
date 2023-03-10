2f565c36ceee593837e5e48e69a8f21039b8fcc5
PARK SUNG JUN <tjdwns9574@gmail.com>
2023-03-10 09:46:12 +0900

    docs : BOJ 1074

---
+++ python/Date_230310_BOJ_1074.py
@@ -0,0 +1,25 @@
+N, r, c = map(int, input().split())
+
+ans = 0
+
+while N != 0:
+
+	N -= 1
+
+	if r < 2 ** N and c < 2 ** N:
+	    pass
+	
+	elif r < 2 ** N and c >= 2 ** N: 
+		ans += ( 2 ** N ) * ( 2 ** N ) * 1
+		c -= ( 2 ** N )
+        
+	elif r >= 2 ** N and c < 2 ** N: 
+		ans += ( 2 ** N ) * ( 2 ** N ) * 2
+		r -= ( 2 ** N )
+        
+	else:
+		ans += ( 2 ** N ) * ( 2 ** N ) * 3
+		r -= ( 2 ** N )
+		c -= ( 2 ** N )
+    
+print(ans)
