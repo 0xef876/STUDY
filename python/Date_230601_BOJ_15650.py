8bea59d22d0dddba7131314d2bc3aa144d3eaf22
PARK SUNG JUN <tjdwns9574@gmail.com>
2023-06-01 10:09:06 +0900

    docs: BOJ 15650

---
+++ python/Date_230601_BOJ_15650.py
@@ -0,0 +1,17 @@
+N ,M = map(int,input().split())
+data = [i for i in range(1,N+1)]
+
+def backtracking(data,visited):
+	if len(visited) == M:
+		print(*visited)
+		return
+	for i in range(len(data)):
+		if data[i] in visited:
+			continue
+		if len(visited) > 0 and data[i] < visited[-1]:
+			continue
+		visited.append(data[i])
+		backtracking(data,visited)
+		visited.pop()
+
+backtracking(data,[])
